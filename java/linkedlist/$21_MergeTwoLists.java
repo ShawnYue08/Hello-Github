4c4dc360db192cbf96b0f1f6d274aaf9774e91fd
ShawnYue08 <46743195+ShawnYue08@users.noreply.github.com>
2019-09-05 21:47:55 +0800

    Create $21_MergeTwoLists.java

---
+++ java/linkedlist/$21_MergeTwoLists.java
@@ -0,0 +1,34 @@
+public class MergeTwoLists {
+  //循环
+  public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+    ListNode res = new ListNode(-1);
+    ListNode cur = res;
+    while(l1 != null && l2 != null) {
+      if(l1.val <= l2.val) {
+        cur.next = l1;
+        l1 = l1.next;
+      } else {
+        cur.next = l2;
+        l2 = l2.next;
+      }
+      cur = cur.next;
+    }
+    cur.next = l1 == null ? l2 : l1;
+    return res.next;
+  }
+  
+  //递归
+  public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+    if(l1 == null) { 
+      return l2;
+    } else if(l2 == null) { 
+      return l1; 
+    } else if(l1.val <= l2.val) {
+        l1.next = mergeTwoLists(l1.next, l2);
+        return l1;
+    } else {
+        l2.next = mergeTwoLists(l1, l2.next);
+        return l2;
+    }
+  }
+}

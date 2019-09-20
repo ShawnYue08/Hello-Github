879df4754a986aa61388ad929744942cce9f3396
ShawnYue08 <46743195+ShawnYue08@users.noreply.github.com>
2019-09-20 15:02:10 +0800

    Update 05_MergeSortedArray.java

--- java/array/05_MergeSortedArray.java
+++ java/array/05_MergeSortedArray.java
@@ -23,4 +23,17 @@ public class 05_MergeSortedArray {
     }
     
     //原地修改
+    public void mergeSortedArray2(int[] nums1, int m, int[] nums2, int n) {
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;
+        while(i >= 0 && j >= 0) {
+            if(nums1[i] > nums2[j]) {
+                nums1[k--] = nums1[i--];
+            } else {
+                nums1[k--] = nums2[j--];
+            }
+        }
+        System.arraycopy(nums2, 0, nums1, 0, j+1);
+    }
 }

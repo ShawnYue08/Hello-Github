37c03b55d9ae0c1d37a628bfef873bcb62f7bc84
ShawnYue08 <46743195+ShawnYue08@users.noreply.github.com>
2019-09-20 11:31:30 +0800

    Update 01_TwoSum.java

--- java/array/01_TwoSum.java
+++ java/array/01_TwoSum.java
@@ -1,6 +1,7 @@
 import java.util.HashMap;
 public class 01_TwoSum {
     public int[] twoSum(int[] nums, int target) {
+        //两数之和：哈希表
         HashMap<Integer,Integer> map = new HashMap<>();
         for(int i = 0; i < nums.length; i++) {
             int complement = target - nums[i];

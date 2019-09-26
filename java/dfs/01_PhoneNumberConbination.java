34ffb1dffb1df0e1d8ae61ca30272822a77fe20c
ShawnYue08 <46743195+ShawnYue08@users.noreply.github.com>
2019-09-26 22:19:01 +0800

    Update 01_PhoneNumberConbination.java

--- java/dfs/01_PhoneNumberConbination.java
+++ java/dfs/01_PhoneNumberConbination.java
@@ -2,10 +2,11 @@ public class 01_PhoneNumberConbination {
     String[] chars = new String[]{"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
     
     public List<String> letterCombinations(String digits) {
-        List<String> state = new ArrayList<>();
+        List<String> state = new ArrayList<>(); 
+        //起先state状态变量是一个空集合，首先肯定是先把单个数字对应的字符装进集合，但是是now集合
         if(digits == null || digits.length() == 0) { return state; }        
-        for(char digit : digits.toCharArray()) {
-            //每到一个新的字母now集合都是新的集合
+        for(char digit : digits.toCharArray()) { 
+            //每到一个新的字母，now集合都是新的集合，这样可以对原先的state集合更新，每加完一个字母进集合，state = now
             List<String> now = new ArrayList<>();
             if(state.size() == 0) {
                 for(char c : chars[digit - '2'].toCharArray()) {

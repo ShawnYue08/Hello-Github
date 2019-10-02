e6338ed65aba817817d7d8d04ed0e7cc314419f9
ShawnYue08 <46743195+ShawnYue08@users.noreply.github.com>
2019-10-02 21:47:52 +0800

    Update 06_MaximumSubarray.java

--- java/dp/06_MaximumSubarray.java
+++ java/dp/06_MaximumSubarray.java
@@ -1,14 +1,11 @@
 public class 06_MaximumSubarray {
     public int maxSubarray(int[] nums) {
-        //f[i]代表以i结尾的子段的最大子序和，
+        //f[i]代表以i结尾的子段，就是子序必须包含nums[i]，当然了，以i结尾的子序，最大和不一定就要包含nums[i]，所以res = max(res, now)。
         int res = Integer.MIN_VALUE;
         int last = 0;
-        //last变量代表f[i-1]
         for(int i = 0; i < nums.length; i++) {
             int now = Math.max(last, 0) + nums[i];
-            //f[i] = max(f[i-1], 0) + nums[i]，
             res = Math.max(res, now);
-            //res = max(f[0], f[1], f[2], ... f[n-1])
             last = now;
         }
         return res;

b69590e19ebd7164d55aee43794efa1f7ffca3a0
ShawnYue08 <46743195+ShawnYue08@users.noreply.github.com>
2019-10-08 11:22:17 +0800

    Update 01_SearchInsertPosition.java

--- java/binarysearch/01_SearchInsertPosition.java
+++ java/binarysearch/01_SearchInsertPosition.java
@@ -1,6 +1,8 @@
 public class 01_SearchInsertPosition {
     public int searchPosition(int[] nums, int target) {
-        //搜索插入位置，搜索大于等于target的最小下标进行插入，搜索的是右区间的最左边界，使用二分查找的模板1
+        //搜索插入位置，搜索大于等于target的最左元素的下标，搜索的是右区间的最左边界，使用二分查找的模板1
+        
+        //假如排序数组的最大元素小于target,那么插入nums.length位置。
         if(nums[nums.length-1] < target) {
             return nums.length;
         }
@@ -8,10 +10,10 @@ public class 01_SearchInsertPosition {
         while(i < j) {
             int mid = i + j >> 1;
             if(nums[mid] >= target) {
-                //当mid满足右侧性质时
+                //如果mid满足右侧性质
                 j = mid;
             } else {
-                //当mid满足左侧性质时
+                //如果mid满足左侧性质
                 i = mid + 1;
             }
         }

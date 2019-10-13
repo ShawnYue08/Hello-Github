b87a0933426352c2015d36dd1370bdd7fe7c5b6e
ShawnYue08 <46743195+ShawnYue08@users.noreply.github.com>
2019-10-13 16:04:21 +0800

    Update and rename 08_DifferentRouteII.java to 08_DifferentRoute.java

--- java/dp/08_DifferentRoute.java
+++ java/dp/08_DifferentRoute.java
@@ -1,3 +1,21 @@
+//不同路径I：没有障碍物的情况
+public class DifferentRouteI {
+    public int uniquePaths(int m, int n) {
+        int[][] f = new int[m][n];
+        f[0][0] = 1;
+        for(int i = 0; i < m; i++) 
+            for(int j = 0; j < n; j++) {
+                //从上方下来
+                if(i > 0)
+                    f[i][j] += f[i-1][j];
+                //从左侧过来
+                if(j > 0)
+                    f[i][j] += f[i][j-1];
+            }
+        return f[m-1][n-1];
+    }
+}
+//不同路径II：存在障碍物的情况
 public class 08_DifferentRouteII {
     public int uniquePathsWithObstacles(int[][] array) {
         //1、f[i][j]数组代表当前[i,j]位置有多少种路径，初始化二位数组默认值为0
@@ -22,3 +40,4 @@ public class 08_DifferentRouteII {
         return f[n-1][m-1];
     }
 }
+

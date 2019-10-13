ae470d27606e833565fa5820fcc792ecb6968d41
ShawnYue08 <46743195+ShawnYue08@users.noreply.github.com>
2019-10-13 12:23:20 +0800

    Update 02_HouseRobber.java

--- java/dp/02_HouseRobber.java
+++ java/dp/02_HouseRobber.java
@@ -1,9 +1,9 @@
 public class HouseRobber {
+    //一个状态数组maxSum[i]
     public int houseRoober(int[] nums) {
-        //打家劫舍maxSum[i]=Math.max(maxSum[i-1], maxSum[i-2]+curr)
-        //maxSum[i]代表当小偷顺序偷到这家时，在不触碰报警装置的前提下所能偷得的最大金额
-        if(nums == null) {return 0;}
-        else if(nums.length == 1) {return nums[0];}
+        //maxSum[i]代表所能偷得的最大金额
+        if(nums == null || nums.length == 0) return 0;
+        else if(nums.length == 1) return nums[0];
         int[] maxSum = new int[nums.length];
         maxSum[0] = nums[0];
         maxSum[1] = Math.max(nums[0], nums[1]);
@@ -11,5 +11,19 @@ public class HouseRobber {
             maxSum[i] = Math.max(maxSum[i-1], maxSum[i-2] + nums[i]);
         }
         return maxSum[maxSum.length-1];
+    }
+    //两个状态数组f[i],g[i]
+    public int houseRoober(int[] nums) {
+        //f[i]代表所有没有选择nums[i]的最大值，f[i] = max(f[i-1], g[i-1])，g[i]代表所有选择nums[i]的最大值，g[i] = nums[i] + f[i-1]
+        //return max(f[nums.length-1], g[nums.length-1])
+        int n = nums.length;
+        int[] f = new int[n], g = new int[n];
+        f[0] = 0;
+        g[0] = nums[0];
+        for(int i = 1; i < n; i++) {
+            f[i] = Math.max(f[i-1], g[i-1]);
+            g[i] = f[i-1] + nums[i];
+        }
+        return Math.max(f[n-1], g[n-1]);
     }
 }

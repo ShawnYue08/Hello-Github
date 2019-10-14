dfc41226b5720503eabba7e7455e8728ee56d5c8
ShawnYue08 <46743195+ShawnYue08@users.noreply.github.com>
2019-10-14 18:14:13 +0800

    Update 01_ClimbingStairs.java

--- java/dp/01_ClimbingStairs.java
+++ java/dp/01_ClimbingStairs.java
@@ -1,12 +1,11 @@
 public class ClimbingStairs {
-    public int climbingStairs(int n) {
-        if(n == 1) { return 1; }
-        int[] dp = new int[n + 1];
-        dp[1] = 1;
-        dp[2] = 2;
-        for(int i = 3; i <=n; i++) {
-            dp[i] = dp[i-1] + dp[i-2];
-        }
-        return dp[n];
+    public int climbStairs(int n) {
+        //最后一步:爬一阶台阶f[i] = f[i-1],爬两阶台阶f[i] = f[i-2],
+        //f[i] = f[i-1] + f[i-2]
+        int[] f = new int[n+1];
+        f[0] = f[1] = 1;
+        for(int i = 2; i <= n; i++)
+            f[i] = f[i-1] + f[i-2];
+        return f[n];
     }
 }

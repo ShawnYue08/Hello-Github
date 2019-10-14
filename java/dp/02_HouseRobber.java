a8f38cead521d39817935353f214198627cc8921
ShawnYue08 <46743195+ShawnYue08@users.noreply.github.com>
2019-10-14 18:31:40 +0800

    Update 02_HouseRobber.java

--- java/dp/02_HouseRobber.java
+++ java/dp/02_HouseRobber.java
@@ -1,29 +1,31 @@
-public class HouseRobber {
-    //一个状态数组maxSum[i]
+//相邻的房屋不能偷，会触动报警装置
+public class HouseRobberI {
+    
     public int houseRoober(int[] nums) {
-        //maxSum[i]代表所能偷得的最大金额
-        if(nums == null || nums.length == 0) return 0;
-        else if(nums.length == 1) return nums[0];
-        int[] maxSum = new int[nums.length];
-        maxSum[0] = nums[0];
-        maxSum[1] = Math.max(nums[0], nums[1]);
-        for(int i = 2; i < nums.length; i++) {
-            maxSum[i] = Math.max(maxSum[i-1], maxSum[i-2] + nums[i]);
-        }
-        return maxSum[maxSum.length-1];
-    }
-    //两个状态数组f[i],g[i]
-    public int houseRoober(int[] nums) {
-        //f[i]代表所有没有选择nums[i]的最大值，f[i] = max(f[i-1], g[i-1])，g[i]代表所有选择nums[i]的最大值，g[i] = nums[i] + f[i-1]
-        //return max(f[nums.length-1], g[nums.length-1])
+        //f[i] = max(f[i-1], f[i-2] + nums[i])
+        if(nums.length == 0) return 0;
         int n = nums.length;
-        int[] f = new int[n], g = new int[n];
-        f[0] = 0;
-        g[0] = nums[0];
-        for(int i = 1; i < n; i++) {
-            f[i] = Math.max(f[i-1], g[i-1]);
-            g[i] = f[i-1] + nums[i];
+        int[] f = new int[n+1];
+        f[1] = nums[0];
+        for(int i = 2; i <= n; i++) {
+            f[i] = Math.max(f[i-1], f[i-2] + nums[i-1]);
         }
-        return Math.max(f[n-1], g[n-1]);
+        return f[n];
     }
+    
+    public int houseRoober(int[] nums) {
+        //根据最后一家是否偷：f[i]最后一家不偷的最大值;g[i]最后一家偷的最大值
+        //f[i] = max(f[i-1], g[i-1])(当最后一家不偷时,前一家可偷可不偷)
+        //g[i] = f[i-1] + nums[i](当最后一家偷时,那么就是前一家不偷 + 当前这家的金额)
+        int n = nums.length;
+        int[] f = new int[n+1], g = new int[n+1];
+        
+        //f[n]代表最后一家，f[1]代表第一家，nums[0]的情况
+        for(int i = 1; i <= n; i++) {
+            f[i] = Math.max(f[i-1], g[i-1]);
+            g[i] = f[i-1] + nums[i-1];
+        }
+        return Math.max(f[n], g[n]);
+    }
+    
 }

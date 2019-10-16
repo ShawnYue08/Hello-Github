ac91bbc7e38c559a3d3c6681f0557d6df934ce17
ShawnYue08 <46743195+ShawnYue08@users.noreply.github.com>
2019-10-16 15:19:16 +0800

    Update 01_ClimbingStairs.java

--- java/dp/01_ClimbingStairs.java
+++ java/dp/01_ClimbingStairs.java
@@ -1,8 +1,8 @@
 public class ClimbingStairs {
     public int climbStairs(int n) {
-        //最后一步:爬一阶台阶f[i] = f[i-1],爬两阶台阶f[i] = f[i-2],
-        //f[i] = f[i-1] + f[i-2]
+        //最后一步可以由f[n-1]和f[n-2]爬一阶或者两阶而来,一维状态数组f[n] = f[n-1] + f[n-2]
         int[] f = new int[n+1];
+        //f[i]代表爬到第i阶楼梯的总方法
         f[0] = f[1] = 1;
         for(int i = 2; i <= n; i++)
             f[i] = f[i-1] + f[i-2];

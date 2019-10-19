eff4f1ba43d48759fd3f8bb7c092f04aea941f02
ShawnYue08 <46743195+ShawnYue08@users.noreply.github.com>
2019-10-19 22:03:43 +0800

    Update $2_AddTwoNumbers.java

--- java/linkedlist/$2_AddTwoNumbers.java
+++ java/linkedlist/$2_AddTwoNumbers.java
@@ -1,29 +1,25 @@
 public class $2_AddTwoNumbers {
 	public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode res = new ListNode(-1);
-        ListNode p = l1, q = l2, current = res;
-        int carry = 0; //进位
+        ListNode dummy = new ListNode(0);
+        ListNode cur = dummy, p1 = l1, p2 = l2;
         int sum = 0;
-	/*
-	两数之和，while(p != null || q != null) 归并两条链表，while(p != null && q != null)
-	两数之和：两条链表某条链表可以为null，值val=0即可，归并两条链表：两条链表每一条都不能为null，当为null时，设置为什么值都不合适，退出
-	while循环时，直接将某条链表链接到res链表上
-	*/
-        while(p != null || q != null) {
-            int m = p != null ? p.val : 0;
-            int n = q != null ? q.val : 0;
-            sum = m + n + carry;
-            current.next = new ListNode(sum % 10);
-            carry = sum / 10;
-	    //只有先判断p/q != null才可以p = p.next, q = q.next
-            if(p != null) { p = p.next; }
-            if(q != null) { q = q.next; }
-            current = current.next;
+        while(p1 != null || p2 != null) {
+            if(p1 != null) {
+                sum += p1.val;
+                p1 = p1.next;
+            }
+            if(p2 != null) {
+                sum += p2.val;
+                p2 = p2.next;
+            }
+            cur.next = new ListNode(sum % 10);
+            sum /= 10;
+            cur = cur.next;
         }
-        //当p和q的链表都只有一个数时，比如 5 + 5 ，这时current指向val=0的个位数的节点
-        if(carry != 0) {
-            current.next = new ListNode(carry);
+        if(sum == 1) {
+            //两数之和，进位最大是1吧，在循环的末尾已经sum /= 10
+            cur.next = new ListNode(1);
         }
-        return res.next; //返回第一个节点
+        return dummy.next; //返回结果的第一个节点
     }
 }

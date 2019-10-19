80c08dd803a464b303a9e9288aa665b9ea3bfc00
ShawnYue08 <46743195+ShawnYue08@users.noreply.github.com>
2019-10-19 21:53:23 +0800

    Update 01_TwoSum.java

--- java/array/01_TwoSum.java
+++ java/array/01_TwoSum.java
@@ -1,15 +1,22 @@
 import java.util.HashMap;
 public class 01_TwoSum {
     public int[] twoSum(int[] nums, int target) {
-        //两数之和：哈希表
+        int[] res = new int[]{-1, -1};
+        //判断边界条件
+        if(nums == null || nums.length < 2) {
+            return res;
+        }
         HashMap<Integer,Integer> map = new HashMap<>();
         for(int i = 0; i < nums.length; i++) {
             int complement = target - nums[i];
             if(map.containsKey(complement)) {
-                return new int[]{map.get(complement), i};
+                res[0] = map.get(complement);
+                res[1] = i;
+                break;
+                //一旦找到正确的下标索引，退出本层for循环
             }
             map.put(nums[i], i);
         }
-        return new int[2];
+        return res;
     }
 }
